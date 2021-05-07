9df6ac1f31a71ec6ffbdcd341774fbcc05f6daf6
Rory Jarrard <rjarrard@frontsteps.com>
2021-05-07 13:16:49 -0600

    determine and save color rgb values

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,6 +6,15 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  @doc """
+  Pick a color (rgb) from first 3 values in image
+  Input: image => %Identicon.Image{ hex: [] }
+  """
+  defp pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+     %Identicon.Image{image | color: {r, g, b}}
   end
 
   defp hash_input(input) do
