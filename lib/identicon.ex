81df474718905acd21deb78e2726d5d4f5a66a7f
Rory Jarrard <rjarrard@frontsteps.com>
2021-05-07 14:29:50 -0600

    create and save grid to struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,9 +10,15 @@ defmodule Identicon do
     |> build_grid
   end
 
-  def build_grid(%Identicon.Image{hex: hex } = image) do
-    hex
-    |> Enum.chunk_every(3, 3, :discard)
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
   end
 
   @doc """
@@ -35,12 +41,13 @@ defmodule Identicon do
   Input: image => %Identicon.Image{ hex: [] }
   """
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-     %Identicon.Image{image | color: {r, g, b}}
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
